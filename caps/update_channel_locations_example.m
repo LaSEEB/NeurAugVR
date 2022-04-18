51beeaa66430d24bf22271cb26ed907488a85b37
varjak <gmlcaetano@gmail.com>
2022-04-18 17:19:34 +0100

    added chanloc file and example

---
+++ caps/update_channel_locations_example.m
@@ -0,0 +1,24 @@
+%% Add
+addpath('C:/Users/guta_/Documents/MATLAB/eeglab2021.0')
+varsbefore = who; eeglab; close; varsnew = []; varsnew = setdiff(who, varsbefore); clear(varsnew{:})
+
+%% Load
+chanlocs_path = 'chanlocsMR32.mat';
+data_path = 'EEG.mat';  % Just as an example: this file does not exist on github
+load(chanlocs_path,'chanlocs');
+load(data_path,'EEG');
+
+%% Update
+if isempty(EEG.chanlocs)  % Insert chanloc struct
+    EEG.chanlocs = chanlocs;
+else
+    chanlocs_temp = [];
+    for chi = 1:numel(EEG.chanlocs)  % Insert chanloc struct matching the label order of already existing EEG.chanloc
+        chanlocs_temp = [chanlocs_temp, chanlocs(ismember({chanlocs(:).labels}, EEG.chanlocs(chi).labels))];
+    end
+    
+    EEG.chanlocs = chanlocs_temp;
+end
+
+%% Check
+EEG = eeg_checkset(EEG);
